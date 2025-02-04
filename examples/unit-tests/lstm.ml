e1f6dd74da677c0e785ff16f538022dc25fef311
Samuel Mimram <smimram@gmail.com>
2025-02-04 21:23:26 +0100

    Ensure backpropagation.

--- examples/unit-tests/lstm.ml
+++ examples/unit-tests/lstm.ml
@@ -28,8 +28,10 @@ let () =
     ref @@ Vector.zero hidden_size
   in
   let net s x = Net.Vector.RNN.long_short_term_memory ~weight_state ~weight ~bias s x in
+  let backpropagated = ref false in
   let s0 = Net.cst (Vector.uniform hidden_size, Vector.uniform hidden_size) in
-  let x0 = Net.observe_descent (fun _ -> print_endline "Backpropagated!") @@ Net.cst @@ Vector.uniform inputs in
+  let x0 = Net.observe_descent (fun _ -> print_endline "Backpropagated!"; backpropagated := true) @@ Net.cst @@ Vector.uniform inputs in
   let s, y = net s0 x0 in
   Net.Vector.drop_pair s;
-  Net.Vector.squared_distance_to (Vector.uniform hidden_size) y |> Net.descent 0.1 |> Net.run
+  Net.Vector.squared_distance_to (Vector.uniform hidden_size) y |> Net.descent 0.1 |> Net.run;
+  assert !backpropagated
