312f3817cc267742a6265dd4f98b0dc2a2e2f140
Samuel Mimram <smimram@gmail.com>
2025-02-04 21:12:25 +0100

    Observe descent.

--- examples/lstm.ml
+++ examples/lstm.ml
@@ -29,7 +29,7 @@ let () =
   in
   let net s x = Net.Vector.RNN.long_short_term_memory ~weight_state ~weight ~bias s x in
   let s0 = Net.cst (Vector.uniform hidden_size, Vector.uniform hidden_size) in
-  let x0 = Net.cst @@ Vector.uniform inputs in
+  let x0 = Net.observe_descent (fun _ -> print_endline "Backpropagated!") @@ Net.cst @@ Vector.uniform inputs in
   let s, y = net s0 x0 in
   Net.Vector.drop_pair s;
   Net.Vector.squared_distance_to (Vector.uniform hidden_size) y |> Net.descent 0.1 |> Net.run
