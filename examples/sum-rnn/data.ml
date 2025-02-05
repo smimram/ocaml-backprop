4bfb896c8994bd91e2e8dda1b7fc1c75d76bdc69
Jérémy Dubut <dubutjeremy@gmail.com>
2025-02-05 15:27:14 +0100

    generating data

--- examples/sum-rnn/data.ml
+++ examples/sum-rnn/data.ml
@@ -1,17 +1,14 @@
+let vocabulary_size = 12
+
 let generate_sums dataset_size sequence_length max_num =
   
-  let x = Array.init (dataset_size*sequence_length)
-    (fun _ -> Random.int max_num) in
-  let y = Array.init dataset_size 
-    (fun n -> 
-      let start = n*sequence_length in
-      let s = ref 0 in
-      for i = 0 to sequence_length-1 do
-        s := !s + x.(start+i)
-      done;
-      !s
-      ) in
-  x, y
+  let x = Array.init dataset_size
+    (fun _ -> 
+      Array.init sequence_length 
+        (fun _ -> Random.int max_num))
+  in
+  let y = Array.map (Array.fold_left (+) 0) x in
+  x,y
 
 let pad n s =
   s ^ String.make (n - String.length s) ' '
@@ -23,20 +20,45 @@ let generate_string_data dataset_size sequence_length max_num =
   let x, y = generate_sums dataset_size sequence_length max_num in
   let max_size_x = sequence_length - 1 + 
     (int_lenght max_num)*sequence_length in
-  let x = Array.init dataset_size
-    (fun n -> 
-      let start = n*sequence_length in
-      let s = ref @@ string_of_int x.(start) in
+  let x = Array.map 
+    (fun x -> 
+      let s = ref @@ string_of_int x.(0) in
       for i = 1 to sequence_length-1 do
-        s := !s ^ "+" ^ string_of_int x.(start+i)
+        s := !s ^ "+" ^ string_of_int x.(i)
       done;
       pad max_size_x !s
-      ) in
+      ) x in
   let max_size_y = int_lenght max_num*sequence_length in
   let y = Array.map 
       (fun x ->
         pad max_size_y @@ string_of_int x
         ) 
       y in
+  x,max_size_x,y,max_size_y
+
+let char_to_index c =
+  match c with
+  | ' ' -> 0
+  | '+' -> 1
+  | '0'..'9' -> Char.code c - 46
+  | _ ->  invalid_arg "Must be a numeral, a space, or a +"
+
+let unit i =
+  assert (i < vocabulary_size);
+  let a = Array.make vocabulary_size 0. in
+  a.(i) <- 1.;
+  a
+
+let string_array_to_array_array_array size =
+  Array.map 
+  (fun s -> 
+    Array.init size (fun n -> unit @@ char_to_index s.[n]))
+
+let generate_data dataset_size sequence_length max_num =
+  let x,max_x,y,max_y = generate_string_data dataset_size sequence_length max_num in
+  let x = string_array_to_array_array_array max_x x in
+  let y = string_array_to_array_array_array max_y y in
   x,y
+
+
   
--- examples/sum-rnn/sum.ml
+++ examples/sum-rnn/sum.ml
@@ -5,10 +5,22 @@ let () =
   let sequence_length = 2 in
   let max_num = 100 in
   Random.self_init ();
-  let x, y = Data.generate_string_data dataset_size sequence_length max_num in
-  for i = 0 to 3 do
-    Printf.printf "%d: %s|\n" i x.(i)
+  let x, y = Data.generate_data dataset_size sequence_length max_num in
+  for i = 0 to 1 do
+    Array.iteri 
+      (fun n x -> 
+        Printf.printf "%d: [" n;
+        Array.iter (fun x -> Printf.printf "%f " x) x;
+        Printf.printf "]\n"
+      )
+      x.(i)
   done;
-  for i = 0 to 3 do
-    Printf.printf "%d: %s|\n" i y.(i)
+  for i = 0 to 1 do
+    Array.iteri 
+      (fun n x -> 
+        Printf.printf "%d: [" n;
+        Array.iter (fun x -> Printf.printf "%f " x) x;
+        Printf.printf "]\n"
+      )
+      y.(i)
   done
